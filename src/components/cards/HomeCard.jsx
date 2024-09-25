2918ee1dad14edc0761155a78c546b29d23af82e
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-09-25 15:03:14 +0200

    fixed a bug

--- src/components/cards/HomeCard.jsx
+++ src/components/cards/HomeCard.jsx
@@ -1,6 +1,6 @@
 import ViewAllProfilesButton from "../buttons/ViewAllProfilesButton";
 import PostList from "../posts/PostList";
-import ProfilesFilter from "../searchbar/ProfilesFilter";
+import ProfilesFilter from "../Searchbar/ProfilesFilter";
 import { StyledHomeCard, StyledFlex } from "./StyledCard.styles";
 
 
